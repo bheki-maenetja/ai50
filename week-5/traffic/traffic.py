e7f5c0000ea96c2b1ace57ee789e050e8526b075
bheki-maenetja <bhekimaenetja@gmail.com>
2021-08-02 13:04:13 +0200

    re-configured hidden layers - performance still ~50%

--- week-5/traffic/traffic.py
+++ week-5/traffic/traffic.py
@@ -67,7 +67,7 @@ def load_data(data_dir):
         folder_path = os.path.join(main_path, f"{i}")
         for file in os.listdir(folder_path):
             img = cv2.imread(os.path.join(folder_path, file))
-            images.append(np.resize(img, (IMG_HEIGHT, IMG_WIDTH, 3)))
+            images.append(np.resize(img, (IMG_WIDTH, IMG_HEIGHT, 3)))
             labels.append(i)
     
     return (images, labels)
@@ -92,12 +92,11 @@ def get_model():
         # Flattening
         tf.keras.layers.Flatten(),
 
-        # Hidden Layers
-        tf.keras.layers.Dense(768, activation="sigmoid"),
-        tf.keras.layers.Dense(512, activation="sigmoid"),
-
-        # Dropout
+        # Hidden Layers with Dropout
         tf.keras.layers.Dropout(0.1),
+        tf.keras.layers.Dense(NUM_CATEGORIES * 20, activation="sigmoid"),
+        tf.keras.layers.Dropout(0.1),
+        tf.keras.layers.Dense(NUM_CATEGORIES * 10, activation="sigmoid"),
 
         # Output Layer
         tf.keras.layers.Dense(NUM_CATEGORIES, activation="sigmoid")
